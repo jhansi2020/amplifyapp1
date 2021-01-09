63932cb77bfe347b923905152ce9430c630aa528
jhansi2020 <75970217+jhansi2020@users.noreply.github.com>
2021-01-09 18:42:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to CCIOTSkilling</h1>
+        <h1>Welcome to CCIOT Skilling</h1>
       </header>
     </div>
   );
